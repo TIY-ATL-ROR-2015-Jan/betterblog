b9c71b52ee030d0e8c242c8428149b9caa1ac3e1
Brit Butler <redline6561@gmail.com>
2015-02-09 10:43:51 -0500

    Add support for creating new users.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get '/users/new', to: 'users#new'
+  post '/users', to: 'users#create'
+
   resources :users, :only => [:index, :destroy] do
     resources :posts
   end
