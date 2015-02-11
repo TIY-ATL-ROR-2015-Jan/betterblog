b80d27635fcdfa5427207b77990cc6dc8536b74d
Brit Butler <redline6561@gmail.com>
2015-02-11 14:25:07 -0500

    Add Relationship Join Model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150210163111) do
+ActiveRecord::Schema.define(version: 20150211192001) do
 
   create_table "post_tags", force: :cascade do |t|
     t.integer  "post_id"
@@ -26,6 +26,13 @@ ActiveRecord::Schema.define(version: 20150210163111) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer  "follower_id"
+    t.integer  "followed_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "tags", force: :cascade do |t|
