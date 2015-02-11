6063c53715b462b75dc6bb4a9df11ca7da80ac42
Brit Butler <redline6561@gmail.com>
2015-02-11 13:34:17 -0500

    Fix N+1 query problem!

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = @user.posts
+    @posts = @user.posts.includes(:tags)
   end
 
   # GET /posts/1
