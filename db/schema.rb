ac02a65c3c4f17a814ef5c7ceeab578ec92c56d7
Brit Butler <redline6561@gmail.com>
2015-02-24 09:42:35 -0500

    Add comments.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150210163111) do
+ActiveRecord::Schema.define(version: 20150224144149) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "post_id"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "post_tags", force: :cascade do |t|
     t.integer  "post_id"
