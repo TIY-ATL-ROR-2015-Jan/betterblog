64663787b3da3b9f12261dec7889e2095b0807fa
Brit Butler <redline6561@gmail.com>
2015-02-24 10:06:10 -0500

    Comment submission working.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,14 @@
 class CommentsController < ApplicationController
+  before_action :set_user
+
   def create
     @post = Post.find(params[:post_id])
-    # binding.pry
-    # @post.comments.create(params[:comment])
+    @post.comments.create(:body => params[:comment][:body])
+    redirect_to [@user, @post]
+  end
+
+  private
+  def set_user
+    @user = User.find(params[:user_id])
   end
 end
